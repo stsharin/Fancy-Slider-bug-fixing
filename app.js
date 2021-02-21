b9de0d6db33b09a2b9d4cf239b52f3f7cd579fcf
stsharin <stssharin@gmail.com>
2021-02-21 23:57:01 +0600

    resolved

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const showImages = (images) => {
 }
 
 const getImages = (query) => {
+  
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
@@ -38,13 +39,13 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
+  element.classList.toggle('added');
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    sliders.splice(sliders.indexOf(img), 1)
   }
 }
 var timer
@@ -133,3 +134,6 @@ search.addEventListener('keypress', function (event) {
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+const toggleSpinner = () => {
+  spinner.classList.toggle('d-none');
+}
